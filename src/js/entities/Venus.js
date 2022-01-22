6166aa242b7bac84062110fb2adda595252d27a2
mohittk <mvk1407@gmail.com>
2022-01-23 00:02:04 +0530

    feat: added venus.js

---
+++ src/js/entities/Venus.js
@@ -0,0 +1,99 @@
+import * as THREE from "three";
+
+export default class Venus{
+    constructor(scene, camera, renderer, data){
+        this.name = "venus";
+        this.scene = scene;
+        this.camera = camera;
+        this.renderer = renderer;
+        this.data = data;
+        this.radius = 500 * this.data.data[0].radius;
+        this.theta = this.data.data[0].angular_distance;
+        this.inclination = 3.4 * (Math.PI / 180);
+        this.y_distance = this.radius * Math.sin(this.data.data[0].inclination * (Math.PI / 180));
+    }
+
+    init() {
+        const VenusGeometry = new THREE.SphereGeometry(10/ 115.06, 32, 32);
+        const material = new THREE.MeshPhongMaterial({
+            color: 0x8B7D82,
+            map: new THREE.TextureLoader().load("assets/venus_main.jpeg")
+
+        });
+        this.venusSphere = new THREE.Mesh(VenusGeometry, material);
+        this.venusSphere.rotation.y = -90 * (Math.PI / 180);
+        this.venusSphere.position.set(
+            Math.sin(this.theta) * this.radius,
+            this.y_distance,
+            this.radius * Math.cos(this.theta)
+        );
+
+        this.scene.add(this.venusSphere);
+        this.drawTrail();
+
+        
+
+    }
+    drawTrail() {
+        const ellipse = new THREE.EllipseCurve(
+            0,0,this.radius,this.radius, -(1.5*Math.PI + this.theta),
+            -Math.PI * 1.5,
+            false,
+            0
+        );
+
+        const points = ellipse.getPoints(50);
+        for( let i = 0; i < points.length; i++){
+            points[i] = new THREE.Vector3(points[i].x, 0, points[i].y);
+
+        }
+
+        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+        const colors = [];
+        const initialColor = 0x8B7D82;
+        for( let i = 0; i < geometry.attributes.position.count; i++){
+            var color = new THREE.Color(initialColor);
+            color.r = color.r - (color.r / geometry.attributes.position.count) * i;
+            color.g = color.g - (color.g / geometry.attributes.position.count) * i;
+            color.b = color.b - (color.b / geometry.attributes.position.count) * i;
+            colors.push(color.r, color.g, color.b);
+        }
+        geometry.setAttribute(
+            "color",
+            new THREE.BufferAttribute(new Float32Array(colors),3)
+        );
+
+        const Linematerial = new THREE.LineBasicMaterial({
+            vertexColors: THREE.VertexColors,
+            transparent: true,
+        })
+
+        const line = new THREE.Line(geometry, Linematerial);
+        line.rotateX(-this.inclination);
+        line.name = "venusTrail";
+        this.trail = line;
+        this.scene.add(line);
+
+    }
+
+    removeTrail() {
+        var trail = this.scene.getObjectByName(this.trail.name);
+        this.scene.remove(trail);
+    }
+
+    seconds = () =>
+    new Date().getUTCHours() * 3600 +
+    new Date().getUTCMinutes() * 60 +
+    new Date().getUTCSeconds();
+
+    render(){
+         this.venusSphere.position.set(
+             Math.sin(this.theta) * this.radius,
+             this.y_distance,
+             this.radius * Math.cos(this.theta)
+         );
+         this.venusSphere.rotation.y = 
+         80 * (Math.PI / 180) + this.seconds() * ((2 * Math.PI) / (24 * 3600));
+    }
+
+}
